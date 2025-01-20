be3e1a910d30e14bb7117f437267f8ce30626476
Eric Graves <eridgraves@gmail.com>
2025-01-19 19:41:23 -0500

    Switching to use Selenium as target page loads arrivals/departures data as a JSON from a js fetch(). Attempted to use requests to get/post the json used to load the page in my browser, but authentication must happen as returns 401.

--- scraper.py
+++ scraper.py
@@ -1,23 +1,56 @@
 # scraper.py
 # Scrape arrivals table from DCA website (https://www.flyreagan.com/arrivals-and-departures)
 # Departures are behind a toggle button, so need to figure out how to access those later
-import urllib3
 from bs4 import BeautifulSoup
+import time
+from selenium import webdriver
+import requests
 
-#create instance to extract from website
-http = urllib3.PoolManager()
+url = "https://www.flyreagan.com/arrivals-and-departures"
+browser = webdriver.Chrome()
 
-# Declare a variable containing the URL is going to be scrapped 
-URL = 'https://www.flyreagan.com/arrivals-and-departures/'
+browser.get(url)
+time.sleep(3)
+html = browser.page_source
+soup = BeautifulSoup(html, "lxml")
 
-#Extract the data from the website
-r = http.request('GET', URL)
+print("Length of Tables: " + str(len(soup.find_all("table"))))
 
-print("Info returned: " + str(r.info()))
-print("Data returned: " + str(r.data))
+browser.close()
+browser.quit() 
 
-#create a map of the site into beautiful soup framework
-soup = BeautifulSoup(r.data, 'html.parser')
+# TODO: refresh rather than restart browser for subsequent loads
+# driver.navigate().refresh();
+#
+# OR to use the F5 approach to (maybe) save a full reload -- depends on if site takes a If-Modified-Since request 
+#
+# Actions actions = new Actions(driver);
+# actions.keyDown(Keys.CONTROL).sendKeys(Keys.F5).perform();
 
-print("[1]***** **")
-[ print( item.prettify() ) for item in soup.find_all('table') ]
+
+response = requests.post("https://www.flyreagan.com/arrivals-and-departures/json", {
+  "headers": {
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9",
+    "if-modified-since": "Mon, 20 Jan 2025 00:29:58 GMT",
+    "priority": "u=1, i",
+    "sec-ch-ua": "\"Not A(Brand\";v=\"8\", \"Chromium\";v=\"132\", \"Google Chrome\";v=\"132\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "x-requested-with": "XMLHttpRequest"
+  },
+  "referrer": "https://www.flyreagan.com/arrivals-and-departures",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": "",
+  "method": "GET",
+  "mode": "cors",
+  "credentials": "include"
+});
+
+if response.status_code == 200:
+    print(response.text)
+else:
+    print("An error occurred:", response.status_code)
